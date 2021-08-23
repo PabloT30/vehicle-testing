df5ceef5f5870a21dd2d20c7282d52a7d82d28a9
pablot30 <torres.pablo@correounivalle.edu.co>
2021-08-23 16:11:27 -0500

    Reformating file to read OBD2 data.

--- can_bus/obd2-reader.py
+++ can_bus/obd2-reader.py
@@ -1,20 +1,6 @@
-import serial
-
-# ELM327 MINI OBD2 reader with Bluetooth interface.
-
-# Find an open source DBC file https://github.com/commaai/opendbc to convert CAN messages.
-# https://www.youtube.com/watch?v=NvYXtQmOYDw
-# Datasheet: https://www.elmelectronics.com/wp-content/uploads/2016/07/ELM327DS.pdf
-
-# baudarate can be either 9600 or 38400, see page 8 in datasheet.
-# try it using minicom on linux.
-
-# with serial.Serial("/dev/rfcomm0", baudrate=38400) as ser:
-#     ser.write(b'atz')
-#     line = str(ser.readline())  # read a '\n' terminated line.
-#     print(line)
-
-import obd  # https://python-obd.readthedocs.io/en/latest/
+# https://python-obd.readthedocs.io/en/latest/
+# https://github.com/brendan-w/python-OBD
+import obd  # 
 
 # print(obd.OBD)            # main OBD connection class
 # print(obd.Async)          # asynchronous OBD connection class
